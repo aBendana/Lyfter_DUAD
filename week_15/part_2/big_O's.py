55796a82aed52c88be7ae7d1b1db087e8fdf709d
unknown <a.bendana.81@gmail.com>
2025-03-12 16:52:12 -0400

    changes made for week 15

--- week_15/part_2/big_O's.py
+++ week_15/part_2/big_O's.py
@@ -15,17 +15,17 @@ def check_if_lists_have_an_equal(list_a, list_b): # O(n^2)
 
 
 #print_10_or_less_elements
-def print_10_or_less_elements(list_to_print): # O(n)
+def print_10_or_less_elements(list_to_print): # O(1)
 	list_len = len(list_to_print) # O(1)
-	for index in range(min(list_len, 10)): # O(n)
+	for index in range(min(list_len, 10)): # O(1)
 		print(list_to_print[index]) # O(1)
 		
 
 #generate_list_trios
-def generate_list_trios(list_a, list_b, list_c): # O(n^3)
+def generate_list_trios(list_a, list_b, list_c): # O(a*b*c)
 	result_list = [] # O(1)
-	for element_a in list_a: # O(n)
-		for element_b in list_b: # O(n^2)
-			for element_c in list_c: # O(n^3)
+	for element_a in list_a: # O(a)
+		for element_b in list_b: # O(a*b)
+			for element_c in list_c: # O(a*b*c)
 				result_list.append(f'{element_a} {element_b} {element_c}') # O(1)
 				
